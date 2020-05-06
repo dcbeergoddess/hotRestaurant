35935df80a3c646967299a03745dab5a331b35c9
William Rave <wrave@gwu.edu>
2020-05-06 12:28:13 -0400

    Server and reservation result files.

--- server.js
+++ server.js
@@ -22,23 +22,40 @@ const reservations = [];
 const waitingList = [];  
 
 app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "index.html"));// Send back index.html file
+  res.sendFile(path.join(__dirname, "pages/index.html"));// Send back index.html file
 });
 
 app.get("/reservation", function(req, res){
   // Send back the reservation request html file
+  res.sendFile(path.join(__dirname, "pages/reserve.html"));
 });
 
 app.get("/api/tables", function(req, res){
   // Send back json of all current tables reserved
+  res.json(reservations);
 });
 
 app.get("/api/waitinglist", function(req, res){
   // Send back json of everyone on the waiting list
+  res.json(waitingList);
 });
 
 app.post("/api/newreservation", function(req, res){
   // Parse posted data and do whatever we need to do
+  let HTMLPage = "<!DOCTYPE html><head></head><body><p>PLACEHOLDER</p> <a href='http://localhost:3000/'>Back to the main page</a></body></html>";
+  const newReservation = req.body;
+  console.log(newReservation);
+
+  if(reservations.length < 5)
+  {
+    reservations.push(newReservation);
+    res.sendFile(path.join(__dirname,"pages/resSuccess.html"));
+  }
+  else
+  {
+    waitingList.push(newReservation);
+    res.sendFile(path.join(__dirname, "pages/resWaiting.html"));
+  }
 });
 
 app.listen(PORT, function () {
